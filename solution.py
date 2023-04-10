f876fd6189274e2e692bdd4d1bc235a30d9b95c2
BlackArcherr <70406811+BlackArcherr@users.noreply.github.com>
2023-04-10 20:38:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,17 @@
+
 import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 474140315 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    conv_x = x_success/x_cnt
+    conv_y = y_success/y_cnt
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+    z = (conv_x - conv_y) / math.sqrt(p*(1-p)*(1/x_cnt + 1/y_cnt))
+    critical_value = norm.ppf(0.015)  
+
+return abs(z) > critical_value
