183e9c3246e7901c7b4f823c3069144b4106df08
BlackArcherr <70406811+BlackArcherr@users.noreply.github.com>
2023-04-13 23:21:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,9 @@
 
 import pandas as pd
 import numpy as np
-
+import statsmodels
 chat_id = 474140315 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    conv_x = x_success/x_cnt
-    conv_y = y_success/y_cnt
-    p = (x_success + y_success) / (x_cnt + y_cnt)
-    z = (conv_x - conv_y) / math.sqrt(p*(1-p)*(1/x_cnt + 1/y_cnt))
-    critical_value = norm.ppf(0.03)  
-
-return abs(z) > critical_value
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+    z_score, p_value = statsmodels.stats.proportion.proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+    return p_value < 0.06
